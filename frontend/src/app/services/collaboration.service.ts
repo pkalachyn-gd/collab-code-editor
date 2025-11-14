4817912d23570e3c088e1fd5048230cfb97e0a07
Max Sokolov <maxim.sokolov2@burservis.ru>
2025-11-14 17:26:33 +0100

    fix: changing nodejs websocket url

--- frontend/src/app/services/collaboration.service.ts
+++ frontend/src/app/services/collaboration.service.ts
@@ -25,7 +25,11 @@ export class CollaborationService implements OnDestroy {
 
     // We use `any` for awareness, since WebsocketProvider has
     // a complex type that we don't need to fully describe here.
-    this.provider = new WebsocketProvider('ws://localhost:1234', roomName, this.ydoc);
+    this.provider = new WebsocketProvider(
+      'whttps://collab-editor-backend-eqfl.onrender.com',
+      roomName,
+      this.ydoc
+    );
 
     this.awareness = this.provider.awareness;
     this.undoManager = new UndoManager(this.yText);
@@ -34,11 +38,9 @@ export class CollaborationService implements OnDestroy {
   ngOnDestroy(): void {
     if (this.provider) {
       this.provider.destroy();
-      console.log('CollaborationService provider destroyed');
     }
     if (this.ydoc) {
       this.ydoc.destroy();
-      console.log('CollaborationService ydoc destroyed');
     }
   }
 }
