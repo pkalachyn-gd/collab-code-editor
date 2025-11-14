f67e5099f611eda145715f0ce5f0f7852bd7b36d
Max Sokolov <maxim.sokolov2@burservis.ru>
2025-11-14 17:15:01 +0100

    fix/changing-nodejs-websocket--irl

--- frontend/src/app/services/collaboration.service.ts
+++ frontend/src/app/services/collaboration.service.ts
@@ -25,7 +25,11 @@ export class CollaborationService implements OnDestroy {
 
     // We use `any` for awareness, since WebsocketProvider has
     // a complex type that we don't need to fully describe here.
-    this.provider = new WebsocketProvider('ws://localhost:1234', roomName, this.ydoc);
+    this.provider = new WebsocketProvider(
+      'https://collab-editor-backend-eqfl.onrender.com',
+      roomName,
+      this.ydoc
+    );
 
     this.awareness = this.provider.awareness;
     this.undoManager = new UndoManager(this.yText);
